8c336fbbc81d468df0fbf55178054da4fd8ffd39
antony0710 <56539394+antony0710@users.noreply.github.com>
2024-01-18 05:47:13 +0000

    login.html

--- scripts.js
+++ scripts.js
@@ -58,4 +58,55 @@ window.onload = function() {
     const button = document.getElementById("save");
     button.addEventListener("click", get_notes);
     button.addEventListener("click", update_notes);
+  
+
+
+    // Keep update the date and time of the page
+    const update_date_time = () => {
+        var date_time = document.getElementById('date_time');
+        var date = new Date();
+        date_time.innerHTML = date.toLocaleString();
+    };
+    setInterval(update_date_time, 1000);
+
+
+
 };
+
+// window.onload = function(){
+//     // Fetch the fetch to days weather
+//     const server = 'https://weatherapi2021.azurewebsites.net';
+//     const update_weather = () => {
+//         var weatherContainer = document.getElementById('weather');
+//         weatherContainer.innerHTML = ''; // Clear the saved notes 
+//         fetch(server+'/getWeather')
+//         .then(response => response.json())
+//         .then(weather => {
+//             // Display the saved notes
+//             weather.forEach(weather => {
+//                 var weatherElement = document.createElement('div');
+//                 weatherElement.className = 'weather';
+    
+//                 var titleElement = document.createElement('h3');
+//                 titleElement.textContent = weather.title;
+//                 weatherElement.appendChild(titleElement);
+    
+//                 var contentElement = document.createElement('pre');
+//                 contentElement.textContent = weather.content;
+//                 contentElement.style.display = 'none'; // Hide the content initially
+//                 weatherElement.appendChild(contentElement);
+    
+//                 weatherElement.addEventListener('click', () => {
+//                     // Toggle the display of the content when the note is clicked
+//                     contentElement.style.display = contentElement.style.display === 'none' ? 'block' : 'none';
+//                 });
+    
+//                 weatherContainer.appendChild(weatherElement);
+//             });
+//         })
+//         .catch((error) => {
+//             console.error('Error:', error);
+//         });
+//     };
+
+// }
